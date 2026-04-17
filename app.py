f99a4fbb9939d5b53ec6716e0902e8d49c8d0b29
tutkunkemal <kemaltutkun@gmail.com>
2026-04-18 01:34:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -252,4 +252,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port) github da bu kod var,
+    app.run(host="0.0.0.0", port=port)
